e44550db0a3961598c8d7bc45a5445e376832862
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2025-02-21 15:40:01 +0100

    Make apd movies

--- read.py
+++ read.py
@@ -5,12 +5,19 @@ import velocity_estimation as ve
 import cosmoplots as cp
 import matplotlib.pyplot as plt
 
-shot = 1160616016
+shot = 1160616026
 
 ds = xr.open_dataset("data/phantom_{}.nc".format(shot))
 # ds = xr.open_dataset("~/phantom_1160616016.nc")
 # ph_1160616016 goes 1.1 to 1.6
 # ph_1120921007 goes 1.35 to 1.5
+
+t_start, t_end = get_t_start_end(ds)
+print("Data with times from {} to {}".format(t_start, t_end))
+
+t_start = (t_start + t_end) / 2
+t_end = t_start + 0.001
+ds = ds.sel(time=slice(t_start, t_end))
 
 # Running mean
 ds = run_norm_ds(ds, 1000)
@@ -19,12 +26,6 @@ ds = run_norm_ds(ds, 1000)
 box_size = 5
 ds = ds.rolling(x=box_size, y=box_size, center=True, min_periods=1).mean()
 
-t_start, t_end = get_t_start_end(ds)
-print("Data with times from {} to {}".format(t_start, t_end))
-
-t_start = (t_start + t_end) / 2
-t_end = t_start + 0.01
-ds = ds.sel(time=slice(t_start, t_end))
 ds = ds.coarsen(x=4).mean().coarsen(y=4).mean()
 dt = get_dt(ds)
 
