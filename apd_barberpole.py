e7e9981ed436e66b7b645d4851cbafdaab13fc79
Sosnowsky <jlo044@uit.no>
2025-03-17 08:11:43 +0100

    Changes

--- apd_barberpole.py
+++ apd_barberpole.py
@@ -10,12 +10,17 @@ import velocity_estimation as ve
 shot = 1140613026
 
 ds = xr.open_dataset("ds_short.nc")
+t_start, t_end = get_t_start_end(ds)
+t_start = (t_start + t_end) / 2
+t_end = t_start + 0.005
+# ds = ds.sel(time=slice(t_start, t_end))
+
 times = ds["time"].values
+refx, refy = 7, 5
 
 
 def plot_ccfs():
     fig, ax = plt.subplots(4, 1, sharex=True, figsize=(10, 20))
-    refx, refy = 6, 5
     ref_signal = ds["frames"].isel(x=refx, y=refy).values
     delta = 1e-5
 
@@ -40,11 +45,45 @@ def plot_ccfs():
     plt.show()
 
 
-plot_ccfs()
+delta = 1e-5
+eo = ve.EstimationOptions(cc_options=ve.CCOptions(cc_window=delta, minimum_cc_value=0.5, interpolate=True))
+tded = ve.TDEDelegator(ve.TDEMethod.CC, ve.CCOptions(cc_window=delta, minimum_cc_value=0.2, interpolate=True), cache=False)
+
+
+def plot_ccfs_grid(ccf=True):
+    fig, ax = plt.subplots(5, 4, sharex=True, figsize=(10, 20))
+    ref_signal = ds["frames"].isel(x=refx, y=refy).values
+
+    for row in range(5):
+        y = 7 - row
+        for column in range(4):
+            x = column + 5
+            signal = ds["frames"].isel(x=x, y=y)
+            if ccf:
+                time, res = fpp.corr_fun(signal, ref_signal, 5e-7)
+                tau, c, _ = tded.estimate_time_delay((x, y), (refx, refy), ve.CModImagingDataInterface(ds))
+            else:
+                Svals, res, s_var, time, peaks, wait = fpp.cond_av(S=signal, T=ds["time"].values, smin=2, Sref=ref_signal, delta=delta*2)
+                tau, c, _ = tded.estimate_time_delay((x, y), (refx, refy), ve.CModImagingDataInterface(ds))
+
+            window = np.abs(time) < delta
+            ax[row, column].plot(time[window], res[window])
+
+            ax[row, column].set_title("R = {:.2f} Z = {:.2f}".format(ds.R.isel(x=x, y=y), ds.Z.isel(x=x,y=y)))
+            ax[row, column].vlines(0, -10, 10, ls="--")
+            ax[row, column].set_ylim(-0.5, 1.1 * np.max(res))
+            ax[row, column].text(x=delta/2, y = 0.5, s=r"$\tau = {:.2f}$".format(tau*1e6))
+
+    name = "grid_ccf{}{}.eps".format(refx, refy) if ccf else "grid_ca{}{}.eps".format(refx, refy)
+    plt.savefig(name, bbox_inches="tight")
+    plt.show()
+
+
+plot_ccfs_grid()
 
 
 ds_short = ds.isel(x=slice(-4, None))
-refx, refy = 2, 5
+refx = refx - 5
 
 # Extract the reference time series
 s_ref = ds_short.frames.isel(
@@ -153,10 +192,11 @@ im.set_extent((rmin, rmax, zmin, zmax))
 im2 = ax[1].imshow(rotated_blob(result.x, r_grid, z_grid), origin="lower", interpolation="spline16")
 im2.set_extent((rmin, rmax, zmin, zmax))
 
+plt.savefig("2d_ccf_{}{}".format(refx+5, refy), bbox_inches="tight")
 plt.show()
 
 
-pd = ve.estimate_velocities_for_pixel(refx, refy, ve.CModImagingDataInterface(ds_short))
+pd = ve.estimate_velocities_for_pixel(refx, refy, ve.CModImagingDataInterface(ds_short), estimation_options=eo)
 vx, vy = pd.vx, pd.vy
 deltax = ds_corr.R.isel(x=refx, y=refy).values - ds_corr.R.isel(x=refx-1, y=refy).values
 deltay = ds_corr.Z.isel(x=refx, y=refy).values - ds_corr.Z.isel(x=refx, y=refy-1).values
@@ -164,7 +204,6 @@ deltay = ds_corr.Z.isel(x=refx, y=refy).values - ds_corr.Z.isel(x=refx, y=refy-1
 u2 = vx ** 2 + vy ** 2
 taux = (vx * deltax) / u2
 tauy = (vy * deltay) / u2
-
 
 def get_taumax(v, w, dx, dy):
     lx_fit, ly_fit = 0.518, 1.686
@@ -195,6 +234,6 @@ result_u = differential_evolution(
 )
 
 
-print(taux, tauy)
+print(result_u)
 
 
