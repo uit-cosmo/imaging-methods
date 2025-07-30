224ae7daea7ccf986f495e4efd271caa15f68479
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2025-07-30 14:44:59 +0200

    Testing

--- tests/integrated_tests.py
+++ tests/integrated_tests.py
@@ -10,6 +10,8 @@ import numpy as np
 import pytest
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import os
+import velocity_estimation as ve
 
 T = 500
 Lx = 8
@@ -27,7 +29,7 @@ refx, refy = 4, 4
 prep_radius = 1000
 tdca_threshold = 2
 tdca_check_max = 1
-tdca_window = 60
+tdca_window = 150
 tdca_single = True
 contour_treshold = 0.3
 contour_com_smooth = 3
@@ -37,13 +39,15 @@ fit_tilt_p = 0.2
 
 taud_cutoff = 1e6
 taud_nperseg = 1000
+figures_dir = "integrated_tests_figures"
 
 
-def test_size():
+def test_case_a():
     ds = make_2d_realization(
         Lx, Ly, T, nx, ny, dt, K, vx=1, vy=0, lx=1, ly=1, theta=0, bs=bs
     )
     ds = ph.run_norm_ds(ds, prep_radius)
+    ds.to_netcdf("test_a.nc")
     # ph.show_movie(ds.sel(time=slice(100, 110)), "frames", gif_name="test_data.gif")
 
     events, average_ds = ph.find_events_and_2dca(
@@ -62,17 +66,18 @@ def test_size():
         max_displacement_threshold=None,
     )
 
+    contour_file_name = os.path.join(figures_dir, "contours_a.gif")
     ph.show_movie_with_contours(
         average_ds,
         contour_ds,
         "cond_av",
         lims=(0, 3),
-        gif_name="test1.gif",
+        gif_name=contour_file_name,
         interpolation="spline16",
         show=False,
     )
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5, 5))
 
     velocity_ds = ph.get_contour_velocity(
         contour_ds.center_of_mass,
@@ -122,7 +127,8 @@ def test_size():
     )
     ax.text(0.1, 0.8, text, fontsize=6, transform=ax.transAxes, color="white")
 
-    plt.savefig("test.eps", bbox_inches="tight")
+    results_file_name = os.path.join(figures_dir, "results_a.eps")
+    plt.savefig(results_file_name, bbox_inches="tight")
     plt.close(fig)
 
     bp = ph.BlobParameters(
@@ -138,5 +144,12 @@ def test_size():
         lambda_psd=lam,
         number_events=average_ds["number_events"].item(),
     )
-
     print(bp)
+
+    assert np.abs(v_c - 1) < 0.1, "Wrong contour velocity"
+    assert np.abs(w_c - 0) < 0.1, "Wrong contour velocity"
+
+    assert np.abs(v_f - 1) < 0.1, "Wrong TDE velocity"
+    assert np.abs(w_f - 1) < 0.1, "Wrong TDE velocity"
+
+    assert np.abs(taud - 1) < 0.5, "Wrong duration time"
