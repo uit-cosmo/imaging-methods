c9c689b2dda8580eefde411821e16ef9700fbabd
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2025-08-25 13:53:47 +0200

    Added some new shots

--- density_scan/density_scan.py
+++ density_scan/density_scan.py
@@ -1,4 +1,5 @@
 from phantom import PlasmaDischargeManager, PlasmaDischarge
+import numpy as np
 
 manager = PlasmaDischargeManager()
 # Data from the provided table
@@ -15,6 +16,64 @@ discharges = [
     PlasmaDischarge(1110201016, 0.90, 3.69, 0.64, 1.06, 1.345, 0.285, "none", "EDA-H"),
     PlasmaDischarge(
         1110201011, 1.20, 4.32, 0.56, 1.06, 1.255, 0.195, "none", "ELM-free-H"
+    ),
+    PlasmaDischarge(1120814016, 0.743, 1.635, 0.33, 1.3, 1.65, 0.35, "none", "EDA-H"),
+    PlasmaDischarge(1120814018, 0.741, 1.621, 0.33, 1.33, 1.65, 0.32, "none", "EDA-H"),
+    PlasmaDischarge(1120814019, 0.744, 1.601, 0.32, 1.33, 1.65, 0.32, "none", "EDA-H"),
+    PlasmaDischarge(1120814020, 0.745, 1.706, 0.34, 1.33, 1.65, 0.32, "none", "EDA-H"),
+    PlasmaDischarge(1120814021, 0.744, 1.987, 0.40, 1.33, 1.65, 0.32, "none", "EDA-H"),
+    PlasmaDischarge(1120814025, 0.745, 1.572, 0.32, 1.33, 1.65, 0.32, "none", "EDA-H"),
+    PlasmaDischarge(1120814026, 0.745, 1.278, 0.26, 0.95, 1.4, 0.45, "scan", "EDA-H"),
+    PlasmaDischarge(1120814027, 0.743, 1.561, 0.32, 0.95, 1.4, 0.45, "scan", "EDA-H"),
+    PlasmaDischarge(1120814028, 0.74, 1.77, 0.36, 1.15, 1.5, 0.35, "scan", "EDA-H"),
+    PlasmaDischarge(1120814029, 0.746, 1.75, 0.35, 1.15, 1.5, 0.35, "scan", "EDA-H"),
+    PlasmaDischarge(1120814030, 0.744, 1.74, 0.35, 1.15, 1.5, 0.35, "scan", "EDA-H"),
+    PlasmaDischarge(1120814031, 0.743, 1.652, 0.33, 1.15, 1.5, 0.35, "scan", "EDA-H"),
+    PlasmaDischarge(1120814032, 0.747, 1.83, 0.37, 1.15, 1.5, 0.35, "scan", "EDA-H"),
+    PlasmaDischarge(
+        1110201007, 0.93, np.nan, np.nan, 1.10, 1.40, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201010, 0.93, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201011, 1.22, np.nan, np.nan, 1.12, 1.25, np.nan, "none", "ELM-free-H"
+    ),
+    PlasmaDischarge(
+        1110201014, 0.93, np.nan, np.nan, 1.05, 1.31, np.nan, "none", "ELM-free-H"
+    ),
+    PlasmaDischarge(
+        1110201015, 0.91, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201016, 0.92, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201018, 0.93, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201021, 0.93, np.nan, np.nan, 1.23, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201022, 0.73, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201023, 0.74, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201025, 0.73, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201026, 0.73, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201027, 0.73, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201030, 0.73, np.nan, np.nan, 1.05, 1.35, np.nan, "none", "EDA-H"
+    ),
+    PlasmaDischarge(
+        1110201034, 0.51, np.nan, np.nan, 1.11, 1.35, np.nan, "none", "EDA-H"
     ),
 ]
 # Add discharges to manager
