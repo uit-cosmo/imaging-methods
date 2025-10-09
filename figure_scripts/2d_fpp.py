1b52ddd237e3ec9c9b09d65fe5e8bbce03ad5eaa
Sosnowsky <jlo044@uit.no>
2025-10-09 13:26:32 +0200

    2dfpp figures

---
+++ figure_scripts/2d_fpp.py
@@ -0,0 +1,94 @@
+import blobmodel as bm
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
+from scipy.signal import welch
+import fppanalysis as fppa
+
+import cosmoplots as cp
+
+matplotlib_params = plt.rcParams
+cp.set_rcparams_dynamo(matplotlib_params, 1)
+plt.rcParams.update(matplotlib_params)
+
+
+dt = 0.1
+T = int(1e5)
+num_blobs = int(1e5)
+start_index = int(100 / dt)
+taup = 1e10
+
+
+def get_realization(shape_x, taup):
+    # Blobmodel
+    model = bm.Model(
+        Nx=1,
+        Ny=1,
+        Lx=1,
+        Ly=0,
+        dt=dt,
+        T=T,
+        num_blobs=num_blobs,
+        blob_shape=bm.BlobShapeImpl(shape_x, bm.BlobShapeEnum.gaussian),
+        periodic_y=False,
+        t_drain=taup,
+        blob_factory=bm.DefaultBlobFactory(
+            A_dist=bm.DistributionEnum.exp, vy_dist=bm.DistributionEnum.zeros
+        ),
+        verbose=True,
+        t_init=0,
+        one_dimensional=True,  # Sets Ly = 0, Ny = 1, and checks vy = 0, and sets the y blob shape to 1.
+    )
+    ds = model.make_realization(speed_up=True, error=1e-10)
+    ds = ds.isel(t=slice(start_index, int(1e50)))
+    signal = ds.n.isel(x=0, y=0).values
+    return signal
+
+
+def get_analytical_acf(taus, shape, tau_p):
+    tau = tau_p / (1 + tau_p)  # ell = 1, v = 1
+    if shape == bm.BlobShapeEnum.exp:
+        return np.exp(-np.abs(taus) / tau)
+    if shape == bm.BlobShapeEnum.gaussian:
+        return np.exp(-1 / 2 * taus**2)
+    # if shape == bm.BlobShapeEnum.lorentz:
+    #    return 4/(4+taus**2)
+    # if shape == bm.BlobShapeEnum.secant:
+    #    return 2 * taus/(np.exp(taus) - np.exp(-taus))
+    return np.zeros(len(taus))
+
+
+def plot_synthetic_data_acf(shape_x, taup, ax, color=None, label=None):
+    signal = get_realization(shape_x, taup)
+    taus, acf = fppa.corr_fun(signal, signal, dt)
+    window_size = 3
+    mask = np.abs(taus) < window_size
+    taus, acf = taus[mask], acf[mask]
+    ax.plot(taus, acf, color=color, label=label, ls="-")
+    ax.plot(taus, get_analytical_acf(taus, shape_x, taup), color=color, ls="--")
+
+
+fig, ax = plt.subplots(1, 2, figsize=(6, 3))
+
+plot_synthetic_data_acf(
+    bm.BlobShapeEnum.secant, 1e10, ax[0], color="blue", label="No damping"
+)
+plot_synthetic_data_acf(bm.BlobShapeEnum.secant, 1, ax[0], color="red", label="Damping")
+ax[0].legend()
+ax[0].set_ylabel(r"$R_{\tilde{\Phi}}(\triangle_t)$")
+ax[0].set_xlabel(r"$\frac{v}{\ell}\triangle_t$")
+ax[0].set_title("Secant shape")
+
+plot_synthetic_data_acf(
+    bm.BlobShapeEnum.lorentz, 1e10, ax[1], color="blue", label="No damping"
+)
+plot_synthetic_data_acf(
+    bm.BlobShapeEnum.lorentz, 1, ax[1], color="red", label="Damping"
+)
+ax[1].legend()
+ax[1].set_ylabel(r"$R_{\tilde{\Phi}}(\triangle_t)$")
+ax[1].set_xlabel(r"$\frac{v}{\ell}\triangle_t$")
+ax[1].set_title("Lorentz shape")
+
+plt.savefig("2d_fpp_acf_other_shapes.pdf", bbox_inches="tight")
+plt.show()
