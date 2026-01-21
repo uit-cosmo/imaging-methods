2a363b6ffc40fd1c3eb69321c9a514137bebc943
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2026-01-21 16:01:41 +0100

    TODO

--- figure_scripts/scans/barberpole_single.py
+++ figure_scripts/scans/barberpole_single.py
@@ -1,10 +1,10 @@
 import imaging_methods as im
 import matplotlib.pyplot as plt
 from blobmodel import BlobShapeEnum, BlobShapeImpl
-import velocity_estimation as ve
 import os
 import cosmoplots as cp
 import xarray as xr
+import numpy as np
 
 plt.style.use(["cosmoplots.default"])
 plt.rcParams["text.latex.preamble"] = (
@@ -17,7 +17,7 @@ plt.rcParams.update(params)
 
 
 mp = im.get_default_synthetic_method_params()
-mp.position_filter.window_size = 1
+mp.position_filter.window_size = 11
 
 T = 5000
 Lx = 8
@@ -28,23 +28,84 @@ dt = 0.1
 bs = BlobShapeImpl(BlobShapeEnum.gaussian, BlobShapeEnum.gaussian)
 K = 5000
 
-N = 5
 NSR = 0.1
 
 lx, ly = 0.5, 2.0
-theta = 0
-i = 0
+theta = 1.57
+i = 3
 
-file_name = os.path.join(
-    "synthetic_data", "data_{:.2f}_{:.2f}_{:.2f}_{}".format(lx, ly, theta, i)
-)
+def get_simulation_data(lx, ly, theta):
+    alpha = np.pi/8
+    vx_input, vy_input = np.cos(alpha), np.sin(alpha)
+    ds = im.make_2d_realization(
+        Lx,
+        Ly,
+        T,
+        nx,
+        ny,
+        dt,
+        K,
+        vx=vx_input,
+        vy=vy_input,
+        lx=lx,
+        ly=ly,
+        theta=theta,
+        bs=bs,
+    )
+    ds_mean = ds.frames.mean().item()
+    ds = ds.assign(
+        frames=ds["frames"] + ds_mean * NSR * np.random.random(ds.frames.shape)
+    )
+    ds = im.run_norm_ds(ds, mp.preprocessing.radius)
+    ds["v_input"] = vx_input
+    ds["w_input"] = vy_input
+    return ds
+
+file_name = os.path.join("synthetic_data", "data_single.nc ")
+#ds = get_simulation_data(0.5, 2, np.pi/4)
+#ds.to_netcdf(file_name)
 ds = xr.open_dataset(file_name)
+plot_gifs = False
+
 
 dt = im.get_dt(ds)
 dr = im.get_dr(ds)
 
 tdca_params = mp.two_dca
 events, average_ds = im.find_events_and_2dca(ds, mp.two_dca)
+
+contour_ca = im.get_contour_evolution(
+    average_ds.cond_av,
+    mp.contouring.threshold_factor,
+    max_displacement_threshold=None,
+)
+contour_cc = im.get_contour_evolution(
+    average_ds.cross_corr,
+    mp.contouring.threshold_factor,
+    max_displacement_threshold=None,
+)
+
+if plot_gifs:
+    im.movie_dataset(ds.sel(time=slice(500, 510)), gif_name="barberpole_dataset.gif", show=False)
+    im.show_movie_with_contours(
+        average_ds,
+        contour_ca,
+        variable="cond_av",
+        lims=(0, average_ds.cond_av.max().item()),
+        gif_name="ca.gif",
+        interpolation=None,
+        show=False,
+    )
+
+    im.show_movie_with_contours(
+        average_ds,
+        contour_cc,
+        variable="cross_corr",
+        lims=(0, average_ds.cross_corr.max().item()),
+        gif_name="cc.gif",
+        interpolation=None,
+        show=False,
+    )
 
 
 def get_positions_and_mask(
@@ -106,7 +167,7 @@ pos_max_2dcc, mask_max_2dcc = get_positions_and_mask(
     average_ds, "cross_corr", mp, position_method="max"
 )
 
-fig, axes = plt.subplots(1, 2)
+fig, axes = plt.subplots(2, 2, figsize=(5, 5), sharex=True, sharey=True)
 
 v_input, w_input = ds["v_input"].item(), ds["w_input"].item()
 
@@ -119,11 +180,11 @@ print("Input velocities: {:.2f},   {:.2f}".format(v_input, w_input))
 
 def plot_component(i, ax, position_ca, position_cc, mask_ca, mask_cc):
     input_velocity = v_input if i == 0 else w_input
-    ax.plot(position_ca.time, position_ca.values[:, i], color="blue")
-    ax.plot(position_cc.time, position_cc.values[:, i], color="green")
+    ax.scatter(position_ca.time, position_ca.values[:, i], color="blue", s= 0.5)
+    ax.scatter(position_cc.time, position_cc.values[:, i], color="green", s= 0.5)
     ax.plot(
         position_ca.time,
-        position_ca.sel(time=0).values[0] + position_ca.time.values * input_velocity,
+        4 + position_ca.time.values * input_velocity,
         color="black",
         ls="--",
     )
@@ -140,20 +201,24 @@ def plot_component(i, ax, position_ca, position_cc, mask_ca, mask_cc):
         lw=2,
         color="green",
     )
+    ylabel = r"$x(t)$" if i == 0 else r"$y(t)$"
+    ax.set_ylabel(ylabel)
 
 
-# plot_component(0, axes[0], contour_ca.center_of_mass, contour_cc.center_of_mass)
-# plot_component(1, axes[1], contour_ca.center_of_mass, contour_cc.center_of_mass)
-plot_component(0, axes[0], pos_max_2dca, pos_max_2dcc, mask_max_2dca, mask_max_2dcc)
-plot_component(1, axes[1], pos_max_2dca, pos_max_2dcc, mask_max_2dca, mask_max_2dcc)
+plot_component(0, axes[0, 0], pos_2dca, pos_2dca, mask_2dca, mask_2dca)
+plot_component(1, axes[0, 1], pos_2dca, pos_2dca, mask_2dca, mask_2dca)
+plot_component(0, axes[1, 0], pos_max_2dca, pos_max_2dcc, mask_max_2dca, mask_max_2dcc)
+plot_component(1, axes[1, 1], pos_max_2dca, pos_max_2dcc, mask_max_2dca, mask_max_2dcc)
 
-fig, ax = plt.subplots()
+axes[0, 0].set_title("Contouring")
+axes[0, 1].set_title("Contouring")
 
-time_index = 35
-time_value = average_ds.time.isel(time=time_index).item()
-ax.imshow(average_ds.cond_av.isel(time=time_index).values, origin="lower")
+axes[1, 0].set_title("Max track.")
+axes[1, 1].set_title("Max track.")
 
-pos_x, pos_y = pos_max_2dca.sel(time=time_value)
-ax.scatter(pos_x, pos_y)
+axes[1, 0].set_xlabel("Time")
+axes[1, 1].set_xlabel("Time")
+
+plt.savefig("barberpole_trajectories.pdf", bbox_inches="tight")
 
 plt.show()
